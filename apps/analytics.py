ab2f3db5ec5b11a6522c1ff9198f7a9598ce1116
hychua <85439265+hychua@users.noreply.github.com>
2024-06-12 19:06:38 +0800

    Update analytics.py

--- apps/analytics.py
+++ apps/analytics.py
@@ -213,8 +213,8 @@ def get_bottom_performers(filters):
     data =  df_full_data.groupby(filters,as_index=False).agg({'Sale Price':['sum']})
     filters.append('Total Sales')
     data.columns = filters
-    data.sort_values(by=['Total Sales'],ascending=[True],inplace=True)
-    return data.head(10)
+    data.sort_values(by=['Total Sales'],ascending=[False],inplace=True)
+    return data.tail(10)
 
 
 def get_avg_net_profit(filters):    
@@ -248,8 +248,8 @@ def get_bottom_avg_net_profit(filters):
     data = data.groupby(filters,as_index=False).agg({'net_profit':['mean']})
     filters.append('Avg Net Profit')
     data.columns = filters
-    data.sort_values(by=['Avg Net Profit'],ascending=[True],inplace=True)
-    return data.head(10)
+    data.sort_values(by=['Avg Net Profit'],ascending=[False],inplace=True)
+    return data.tail(10)
 
 
 
