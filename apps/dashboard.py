61cbb5d8a68544c06f64ea3583570b9b489a91ce
hychua <85439265+hychua@users.noreply.github.com>
2024-07-03 14:40:39 +0800

    Update dashboard.py

--- apps/dashboard.py
+++ apps/dashboard.py
@@ -349,7 +349,7 @@ layout2 = html.Div(
 
                                 #),
               html.Div([
-                      html.Div([
+                      html.Div([id="app-container-3",
                                 html.Div(
                                     id = "no-id",
                                     children=[
