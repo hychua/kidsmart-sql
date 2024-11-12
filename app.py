ab04fe3887e8ea664963ec15e0db16c034b48771
hychua <85439265+hychua@users.noreply.github.com>
2024-11-12 12:24:07 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
   
 import dash
-
+import dash_bootstrap_components as dbc
 
 external_stylesheets = [ 'https://codepen.io/chriddyp/pen/bWLwgP.css']
 
@@ -12,6 +12,8 @@ app = dash.Dash(__name__,
                 meta_tags=[{'name': 'viewport',
                             'content': 'width=device-width, initial-scale=1.0'}]
                 )
-app.title = 'Sales and Inventory Dashboard'
+app.title = 'Kidsmart Retail Dashboard'
+
+app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
 
 server = app.server
--- index.py
+++ index.py
@@ -1,7 +1,8 @@
-
+import dash
 from dash import dcc
 from dash import html
-from dash.dependencies import Input, Output
+from dash.dependencies import Input, Output, State
+import dash_bootstrap_components as dbc
 
 # from dashboard.py
 from apps import dashboard
@@ -10,67 +11,144 @@ from apps import dashboard
 from app import app
 import os
 
+
 # define tab styles
 tab_style = {
-    'borderBottom': '3px solid #d6d6d6',
-    'padding': '12x',
-    'fontWeight': 'bold',
-    'font_size': '16px'
+    'borderTop': '1px solid #d6d6d6',
+    'borderBottom': '1px solid #d6d6d6',
+    'height': '10rem',
+    'padding': '3.5rem',
+    'font-size': '1.7rem'
 }
 
 tab_selected_style = {
-    'borderTop': '3px solid #d6d6d6',
-    'borderBottom': '3px solid #d6d6d6',
+    'borderTop': '1px solid #d6d6d6',
+    'borderBottom': '1px solid #d6d6d6',
     'backgroundColor': '#000000',
     'color': 'white',
-    'padding': '12px',
-    'font_size': '16px'
+    'height': '10rem',
+    'padding': '3.5rem',
+    'font-size': '1.7rem'
 }
 
-# Create Pages      
-layout_index = html.Div([
+# make a reuseable navitem for the different examples
+inv_item = dbc.NavItem(dbc.NavLink("Inventory |", href="/inventory"))
+sale_item = dbc.NavItem(dbc.NavLink("Sales |", href="/sales"))
+
+# make a reuseable dropdown for the different examples
+dropdown = dbc.DropdownMenu(
+    children=[
+        dbc.DropdownMenuItem("SALE", href="/order", style = {"font-size": "1.5rem"}),
+        dbc.DropdownMenuItem("SALE ITEMS", href="/sale_product", style = {"font-size": "1.5rem"}),
+        dbc.DropdownMenuItem("PO", href="/po", style = {"font-size": "1.5rem"}),
+        dbc.DropdownMenuItem("PO ITEMS", href="/po_product", style = {"font-size": "1.5rem"}),
+        dbc.DropdownMenuItem("PRODUCT", href="/product", style = {"font-size": "1.5rem"}),
+        dbc.DropdownMenuItem("CATEGORY", href="/category", style = {"font-size": "1.5rem"}),
+        dbc.DropdownMenuItem("STORE", href="/store", style = {"font-size": "1.5rem"}),
+        dbc.DropdownMenuItem("REGION", href="/region", style = {"font-size": "1.5rem"}),
+        dbc.DropdownMenuItem("CITY", href="/city", style = {"font-size": "1.5rem"}),
+    ],
+    nav=True,
+    in_navbar=True,
+    label="Upload",
     
-                html.Div([
-                    
-                html.Img(id="logo", src=app.get_asset_url("logo.png")),
-                
-                    #html.Div([ html.H2('Kidsmart Dashboard'),
-       
-                    #],style={'display':'inline-block','margin-left':50}),
-                            
-                        ],
-                        style={}),
+)
 
-    html.Div([
-        dcc.Tabs(id='tabs', value='inventory-dash', children=[
-            dcc.Tab(label='Sales Dashboard', value='sales-dash',style=tab_style, selected_style=tab_selected_style),
-            dcc.Tab(label='Inventory Dashboard', value='inventory-dash',style=tab_style, selected_style=tab_selected_style),
-            dcc.Tab(label='Upload Data', value='upload-data',style=tab_style, selected_style=tab_selected_style),
+# this example that adds a logo to the navbar brand
+navbar = dbc.Navbar(
+    dbc.Container(
+        [
+            html.A(
+                # Use row and col to control vertical alignment of logo / brand
+                dbc.Row(
+                    [
+                        dbc.Col(html.Img(id="logo", src=app.get_asset_url("logo.png"))),
+                        dbc.Col(dbc.NavbarBrand("Kidsmart Dashboard", style = {"font-size": "2rem","padding-left":"1rem"})),
+                    ],
+                align="center",
+                className="g-0",
+                ),
+                href = "https://shopee.ph/megaluck",
+                style={"textDecoration": "none"},
+            ),
+            dbc.NavbarToggler(id="navbar-toggler", n_clicks=0),
+            dbc.Collapse(
+                dbc.Nav(
+                    [inv_item, sale_item, dropdown],
+                    className="ms-auto",
+                    navbar=True,
+                ),
+                id="navbar-collapse",
+                navbar=True,
+            ),
+        ],
+    ),
+    color="dark",
+    dark=True,
+    className="mb-5",
+    style = {"font-size": "2rem"}
+)
 
-        ]),
-    
-    ], style={}),
-    html.Div(id='tabs-content', style={}),
-
-])
-# end create pages
-
-app.layout = layout_index
+app.layout = html.Div(
+    [
+        dcc.Location(id="url", refresh=False, pathname="/inventory"),
+        navbar,
+        dbc.Container(id="page-content", fluid=True),
+    ]
+)
 
 
-# Tab callbacks
-@app.callback(Output('tabs-content', 'children'),
-              Input('tabs', 'value'))
-def render_content(tab):
-    if tab == 'sales-dash':
-        return dashboard.layout1
-    elif tab == 'inventory-dash':
+@app.callback(Output("page-content", "children"), Input("url", "pathname"))
+def display_page(pathname):
+    if pathname == "/inventory":
         return dashboard.layout2
-    elif tab == 'upload-data':   
-        return dashboard.layout3
+    elif pathname == "/sales":
+        return dashboard.layout1
+    elif pathname == "/order":
+        return dashboard.sale_page
+    elif pathname == "/sale_product":
+        return dashboard.sale_product_page
+    elif pathname == "/po":
+        return dashboard.po_page
+    elif pathname == "/po_product":
+        return dashboard.po_product_page
+    elif pathname == "/product":
+        return dashboard.product_page
+    elif pathname == "/category":
+        return dashboard.category_page
+    elif pathname == "/store":
+        return dashboard.store_page
+    elif pathname == "/region":
+        return dashboard.region_page
+    elif pathname == "/city":
+        return dashboard.city_page
+    else:
+        return dbc.Jumbotron(
+            [
+                html.H1("404: Not found", className="text-danger"),
+                html.Hr(),
+                html.P(f"The pathname {pathname} was not recognized..."),
+            ]
+        )
 
 
-app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 10000)))
+# we use a callback to toggle the collapse on small screens
+def toggle_navbar_collapse(n, is_open):
+    if n:
+        return not is_open
+    return is_open
+
+
+# the same function (toggle_navbar_collapse) is used in all three callbacks
+for i in [1, 2, 3]:
+    app.callback(
+        Output(f"navbar-collapse{i}", "is_open"),
+        [Input(f"navbar-toggler{i}", "n_clicks")],
+        [State(f"navbar-collapse{i}", "is_open")],
+    )(toggle_navbar_collapse)
+
+
+#app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 10000)))
 
 if __name__ == '__main__':
     app.run_server(debug=False)
